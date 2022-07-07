030f941c878607db376927ef488b4a7411767faf
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-07-07 14:43:14 +0700

    Bai 93: Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import {Routes , Route, Link} from 'react-router-dom';
-import HomePage from './pages/Home'
-import NewsPage from './pages/News'
-import ContactPage from './pages/Contact'
-
+import { Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/Home';
+import NewsPage from './pages/News';
+import ContactPage from './pages/Contact';
 
 // Tạo component App
 function App() {
-  return (
-      <div style={{ padding: 32 }}>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/news">News</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+    return (
+        <div style={{ padding: 32 }}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
 
-        <Routes >
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/news" element={<NewsPage/>}></Route>
-          <Route path="/contact" element={<ContactPage />}></Route>
-        </Routes>
-      </div>
-  );
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/news" element={<NewsPage />}></Route>
+                <Route path="/contact" element={<ContactPage />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
