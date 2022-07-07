717f07e4f1a63d49d61b78c3732d2fc726002e13
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-07-07 23:41:17 +0700

    Bai 94: Cấu hình sử dụng CSS/SASS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/Home';
-import NewsPage from './pages/News';
-import ContactPage from './pages/Contact';
-
 // Tạo component App
 function App() {
     return (
         <div style={{ padding: 32 }}>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-
-            <Routes>
-                <Route path="/" element={<HomePage />}></Route>
-                <Route path="/news" element={<NewsPage />}></Route>
-                <Route path="/contact" element={<ContactPage />}></Route>
-            </Routes>
+            <h1>React App</h1>
         </div>
     );
 }
