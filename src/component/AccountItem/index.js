0b01b52b9e8f3461fa983a832f3670f93b3cab4b
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-07-10 01:35:32 +0700

    Bai 98: Xây dựng UI phần Account in Search bar

---
+++ src/component/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/17937206d75158670a293f5caa5b6dda~c5_300x300.webp?x-expires=1657562400&x-signature=1N9F4OVy87%2BO%2BT9eFLCPRcrZ%2Fj8%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('usernamne')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
